9fa8d21e6ae3d63216392a2cd1b94ecd571c7a8a
abdulmaleekadams <harbdulmaleek@gmail.com>
2024-08-13 11:55:40 +0100

    Fix for windows

--- run_streamlit.sh
+++ run_streamlit.sh
@@ -1,35 +1,13 @@
 #!/bin/bash
 
-# Create a virtual environment
 python -m venv venv
 
-# Activate the virtual environment
 source ./venv/Scripts/activate
 
-# Install required packages
 pip install -r ./requirements.txt
 
-echo "Packages installed successfully"
+echo "Package installed successfully"
 
-# Run the FastAPI server in the background
-uvicorn main:app --reload &
+fastapi dev main.py
 
-# Wait for the server to start
-sleep 3
-
-# Open the index.html in the default web browser
-
-if which xdg-open > /dev/null
-then
-  xdg-open http://127.0.0.1:8000
-elif which gnome-open > /dev/null
-then
-  gnome-open http://127.0.0.1:8000
-elif which open > /dev/null
-then
-  open http://127.0.0.1:8000
-else
 start http://127.0.0.1:8000
-fi
-
-wait
