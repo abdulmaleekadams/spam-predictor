c75e3087fa0e3df6cd8c09796d2b18db9889fea6
abdulmaleekadams <harbdulmaleek@gmail.com>
2024-08-13 10:20:11 +0100

    Create frontend bash script to run app

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
 
 import pickle
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.sequence import pad_sequences
-
+ 
 with open('tokenizer.pkl', 'rb') as tokenizer_file:
     tokenizer = pickle.load(tokenizer_file)
 
@@ -16,6 +19,15 @@ max_length = 500
 
 
 app = FastAPI()
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Adjust to your needs (e.g., specific domains instead of "*")
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allows all headers
+)
 
 # Load the saved model, tokenizer, and label encoder
 model = load_model('spam_detector_model.keras')
@@ -34,9 +46,11 @@ def predict_spam(email_text):
     print(prediction)
     return 'Spam' if prediction > 0.5 else 'Not Spam'
 
+app.mount("/static", StaticFiles(directory="frontend"), name="static")
+
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+async def read_index():
+    return FileResponse('frontend/index.html')
 
 @app.post("/predict")
 async def predict(email_text: EmailText):
